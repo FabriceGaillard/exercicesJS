62c99bbc1a71765126fa9e2cdd96c8e1a3cf5ba1
Fab <fabou291@gmail.com>
2023-01-01 11:56:16 +0100

    Write a statement of the exercice

---
+++ assets/js/exo1.js
@@ -0,0 +1,23 @@
+const dog = {
+  name: "babka",
+  legs: 4,
+  age: 8,
+  birgthDay: "01/01/2023",
+};
+
+// notation pointé, afficher la valeur de la propriété nommé name
+
+// notation en crochet, afficher à la valeur de la propriété nommé age
+
+// stocker la valeur de la propriété nommée legs
+
+// utiliser cette variable pour récupérer la valeur de la propriété legs
+
+// Déclarer une fonction nommée, n'ayant aucune propriété, ne faisant rien
+
+/* Déclarer une fonction nommée getProperty ayant deux paramètres. 
+Le premier paramètre servira à stocker un objet
+Le deuxième paramètre servira à stocker une chaine de caractère
+La fonction devra retourner la valeur de la propriété égale à la chaine de caractère 
+du deuxieme paramètre de la fonction 
+*/
